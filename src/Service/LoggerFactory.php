45babfce016fa80ff9e1fbd8ece98989cc99a2aa
BoudaDols <dolsombouda@gmail.com>
2026-04-06 16:28:17 -0400

    feat: add stdout JSON logging for cloud compatibility - Add stdout handler with JsonFormatter to LoggerFactory - All modules (app, migration, seeder) now log to both file and stdout - JSON format compatible with AWS CloudWatch, GCP Cloud Logging, Datadog

--- src/Service/LoggerFactory.php
+++ src/Service/LoggerFactory.php
@@ -3,11 +3,20 @@
 
    use Monolog\Logger;
    use Monolog\Handler\StreamHandler;
+   use Monolog\Formatter\JsonFormatter;
 
    class LoggerFactory {
       public function createLogger(string $name): Logger {
          $log = new Logger($name);
+
+         // File handler
          $log->pushHandler(new StreamHandler(__DIR__ . '/../../var/app.log', Logger::DEBUG));
+
+         // Stdout handler for cloud logging (JSON format)
+         $stdoutHandler = new StreamHandler('php://stdout', Logger::DEBUG);
+         $stdoutHandler->setFormatter(new JsonFormatter());
+         $log->pushHandler($stdoutHandler);
+
          return $log;
       }
    }
